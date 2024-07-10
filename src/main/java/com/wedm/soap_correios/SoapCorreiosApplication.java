871c1982288bebdb3395094ea90a31df9cdbb260
Wellington Nascimento <wellington.d.nascimento@uol.com.br>
2024-07-10 16:35:05 -0300

    first commit

---
+++ src/main/java/com/wedm/soap_correios/SoapCorreiosApplication.java
@@ -0,0 +1,13 @@
+package com.wedm.soap_correios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCorreiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCorreiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wedm/soap_correios/SoapCorreiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedm.soap_correios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCorreiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
